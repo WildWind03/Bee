d4f77765484189c6fb8047a1a2c6ac21755fd679
WildWind03 <usergreat03@gmail.com>
2016-12-25 20:43:35 +0700

    Add expanding by shaking

--- app/src/main/java/com/bee/client/base/ProductInfoFragment.java
+++ app/src/main/java/com/bee/client/base/ProductInfoFragment.java
@@ -1,5 +1,15 @@
 package com.bee.client.base;
 
+import android.annotation.TargetApi;
+import android.content.Context;
+import android.content.res.Resources;
+import android.graphics.Color;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.AsyncTask;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.design.widget.AppBarLayout;
@@ -8,6 +18,7 @@ import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 import butterknife.BindView;
 import com.nsu.alexander.apptemplate.BaseFragment;
 import com.nsu.alexander.apptemplate.R;
@@ -17,10 +28,23 @@ import com.bee.client.entity.Product;
 import java.util.ArrayList;
 import java.util.logging.Logger;
 
-public class ProductInfoFragment  extends BaseFragment {
+public class ProductInfoFragment  extends BaseFragment implements SensorEventListener {
     private static final Logger logger = Logger.getLogger(ProductInfoFragment.class.getName());
+
     private static final String COMMENTS_TAG = "COMMENTS_TAG";
     private static final String PRODUCT_TAG = "PRODUCT_TAG";
+
+    private static final float SHAKE_THRESHOLD = 2.7F;
+    private static final int MIN_PERDIOD_BETWEEN_SHAKE_EVENTS = 10;
+    private static final int SHAKE_STOP_TIME = 3000;
+
+    private long lastShakeEventTime;
+    private volatile int shakeCount;
+
+    private SensorManager sensorManager;
+    private Sensor accelerometer;
+
+    private AsyncTask<Void, Integer, Void> asyncTask;
 
     @BindView(R.id.list_of_comments)
     protected RecyclerView listOfComments;
@@ -37,6 +61,45 @@ public class ProductInfoFragment  extends BaseFragment {
     @BindView(R.id.description)
     protected TextView description;
 
+    private class ColorAsyncTask extends AsyncTask<Void, Integer, Void> {
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+
+            try {
+                while(shakeCount > 0 && !isCancelled()) {
+                    Thread.sleep(200);
+                    shakeCount -= 1;
+                    publishProgress();
+                }
+            } catch (InterruptedException e) {
+                logger.info("Interrupted");
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onProgressUpdate(Integer... values) {
+            super.onProgressUpdate(values);
+
+            final int newColor = 255 - shakeCount * 10;
+
+            if (newColor < 0) {
+                return;
+            }
+
+            listOfComments.setBackgroundColor(Color.rgb(255,255, newColor));
+
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            super.onPostExecute(aVoid);
+            asyncTask = null;
+        }
+    }
+
     public static ProductInfoFragment newInstance(ArrayList<Comment> comments, Product product) {
         ProductInfoFragment productInfoFragment = new ProductInfoFragment();
         Bundle args = new Bundle();
@@ -50,6 +113,9 @@ public class ProductInfoFragment  extends BaseFragment {
     @Override
     protected void onPostViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onPostViewCreated(view, savedInstanceState);
+
+        sensorManager = (SensorManager) getActivity().getSystemService(Context.SENSOR_SERVICE);
+        accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
 
         ArrayList<Comment> comments = getArguments().getParcelableArrayList(COMMENTS_TAG);
         Product product = getArguments().getParcelable(PRODUCT_TAG);
@@ -66,7 +132,76 @@ public class ProductInfoFragment  extends BaseFragment {
     }
 
     @Override
+    public void onResume() {
+        super.onResume();
+        sensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_UI);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        sensorManager.unregisterListener(this);
+    }
+
+    @Override
     protected int getLayout() {
         return R.layout.product_info_layout;
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent) {
+        float x = sensorEvent.values[0];
+        float y = sensorEvent.values[1];
+        float z = sensorEvent.values[2];
+
+        float gX = x / SensorManager.GRAVITY_EARTH;
+        float gY = y / SensorManager.GRAVITY_EARTH;
+        float gZ = z / SensorManager.GRAVITY_EARTH;
+
+        double gForce = Math.sqrt(gX * gX + gY * gY + gZ * gZ);
+
+        if (gForce > SHAKE_THRESHOLD) {
+            final long now = System.currentTimeMillis();
+
+            if (lastShakeEventTime + MIN_PERDIOD_BETWEEN_SHAKE_EVENTS > now) {
+                return;
+            }
+
+            if (lastShakeEventTime + SHAKE_STOP_TIME < now) {
+                shakeCount = 0;
+                onShakeEventStopped();
+            }
+
+            lastShakeEventTime = now;
+            shakeCount++;
+
+            onShakeEventHappened(shakeCount);
+        }
+    }
+
+    @TargetApi(Build.VERSION_CODES.M)
+    private void onShakeEventHappened(final int shakeCount) {
+        appBarLayout.setExpanded(false, true);
+        final int newColor = 255 - shakeCount * 10;
+
+        if (newColor < 0) {
+            return;
+        }
+
+        listOfComments.setBackgroundColor(Color.rgb(255,255, newColor));
+
+        if (null == asyncTask) {
+            asyncTask = new ColorAsyncTask();
+            asyncTask.execute();
+        }
+    }
+
+    private void onShakeEventStopped() {
+
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
+
     }
 }
